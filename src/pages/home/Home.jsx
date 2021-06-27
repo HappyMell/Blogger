4367bf2b2ae26ad86de604c3f163c3be0d739b64
Mellisa Madrid <mellisamadrid@hotmail.com>
2021-06-27 14:55:15 +0100

    .

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await axios.get(
-        "https://mellisa-blogger.herokuapp.com/posts" + search
+        "https://mellisa-blogger.herokuapp.com/posts/" + search
       );
       setPosts(res.data);
     };
